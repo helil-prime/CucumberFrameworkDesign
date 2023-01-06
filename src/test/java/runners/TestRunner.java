4551081bfbea064b34595c4c5079118b7157eae5
helil-prime <helil@primetechschool.com>
2023-01-05 19:35:14 -0500

    created amazon search tests

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -10,7 +10,7 @@ import io.cucumber.junit.CucumberOptions;
 		features="./src/test/resources/features",
 		glue="step_definitions",
 		// when it's true, cucumber only runs the scenario steps that have not been implemented.
-		tags="@userManagement",
+		tags="@amazonSearchTests",
 		publish=true
 		)
 public class TestRunner {
